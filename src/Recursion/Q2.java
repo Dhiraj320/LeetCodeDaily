a044035d41805597a0aee421494d2b5426ff8c61
Dhiraj320 <dhirajkumawat320@gmail.com>
2024-09-21 18:10:23 +0530

    lexicalOrder386

--- src/Recursion/Q2.java
+++ src/Recursion/Q2.java
@@ -1,14 +1,18 @@
-package Recursion;/*
-//Approach 1 Recursion -DFS
-//TC =O(n);
-//SC =O(n) // because of recursion stack
+package Recursion;
 
-class Solution {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q2 {
+    /*
+    //Approach (Simple Recursion - DFS)
+//T.C : O(n) - We visit each number (1 to n) only once.
+//S.C : O(d) - where d is the number of digits in n i.e. log10(n)
     public List<Integer> lexicalOrder(int n) {
         List<Integer> result= new ArrayList<>();
-          // Start from numbers 1 to 9
+        // Start from numbers 1 to 9
         for(int startNum=1; startNum<=9; startNum++){
-             dfs(startNum, n, result);
+            dfs(startNum, n, result);
 
         }
         return result;// Return the result list
@@ -20,41 +24,35 @@ class Solution {
         }
         result.add(curNum); // Add current number to result
 
-     // Generate the new number by appending digits 0 to 9
+        // Generate the new number by appending digits 0 to 9
         for(int append=0;append<=9; append++){
             int newNum=(curNum*10) + append;
             if(newNum>n){
                 break;
             }
-                dfs(newNum, n, result); // Recursive call
-            
+            dfs(newNum, n, result); // Recursive call
+
         }
     }
-}
-*/
+    */
 
-//Approach 2 Iterative
+    //Approach 2 Iterative
 //TC =O(n);
 //SC =O(1) // Without extra Space
-
-import java.util.ArrayList;
-import java.util.List;
-
-class Solution {
     public List<Integer> lexicalOrder(int n) {
         List<Integer> result= new ArrayList<>();
         int curNum=1;
-          // Start from numbers 1 to 9
+        // Start from numbers 1 to 9
         for(int startNum=1; startNum<=n; startNum++){
             result.add(curNum);
-           if(curNum*10<=n){
-            curNum=curNum*10;
-           }else{
-            while(curNum%10==9 || curNum==n){
-                curNum=curNum/10;
+            if(curNum*10<=n){
+                curNum=curNum*10;
+            }else{
+                while(curNum%10==9 || curNum==n){
+                    curNum=curNum/10;
+                }
+                curNum=curNum+1;
             }
-            curNum=curNum+1;
-           }
 
         }
         return result;// Return the result list
